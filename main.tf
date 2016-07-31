6499c95377d770f30d6cf0409d77e95cb41af66b
Israel Sotomayor <sotoisra24@gmail.com>
2016-07-31 12:23:35 +0200

    fix: variable name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "mod" {
     name = "${var.name}"
     location = "${var.location}"
-    resource_group_name = "${var.resource_group.name}"
+    resource_group_name = "${var.resource_group_name}"
     account_type = "${var.account_type}"
 }
